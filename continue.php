0a1bce31fc49ec2c007210531b4091bf3f836ad8
gabu <gabujr337@gmail.com>
2025-02-17 11:01:41 +0300

    changed the static pages to .php dynamic pages

--- continue.php
+++ continue.php
@@ -17,7 +17,7 @@
     <form class="pulse animated continue-form">
         <div class="text-center"><img width="100" height="100" src="assets/img/AYLF-LOGO-NB-02.webp"></div>
         <div></div>
-        <div class="continue-btns"><a class="btn btn-primary btn-lg fw-bolder check-out-btn" role="button" data-bss-hover-animate="tada" style="background: #ffdf00;border-style: none;font-family: 'Fredoka One', serif;color: rgb(0,0,0);" href="index.html">check out our programs?</a><a class="btn btn-primary btn-lg fw-bolder another-btn" role="button" data-bss-hover-animate="tada" style="background: #ffdf00;border-style: none;font-family: 'Fredoka One', serif;color: rgb(0,0,0);" href="index.html">Submit Another Question ?</a></div>
+        <div class="continue-btns"><a class="btn btn-primary btn-lg fw-bolder check-out-btn" role="button" data-bss-hover-animate="tada" style="background: #ffdf00;border-style: none;font-family: 'Fredoka One', serif;color: rgb(0,0,0);" href="index.php">check out our programs?</a><a class="btn btn-primary btn-lg fw-bolder another-btn" role="button" data-bss-hover-animate="tada" style="background: #ffdf00;border-style: none;font-family: 'Fredoka One', serif;color: rgb(0,0,0);" href="index.php">Submit Another Question ?</a></div>
         <p style="font-family: 'Fugaz One', serif;font-weight: bold;color: #ffdf00;margin-top: 40px;">Nurturing A New Breed Of Leaders In Africa</p>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
